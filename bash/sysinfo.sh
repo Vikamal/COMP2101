e6f1616d94c939b6981ca4b238eb0c53eec70074
Victoria Malishko <vika0493@gmail.com>
2022-09-21 14:03:32 -0400

    add sysinfo file with information about a computer

---
+++ bash/sysinfo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#FQDN
+hostname --fqdn
+#The operating system name and version
+hostnamectl
+#Ip addresses
+hostname -I
+#he amount of space available in only the root filesystem
+df -h ./
+
+
+exit
