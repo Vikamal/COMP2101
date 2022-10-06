41cd1132b2cdf756c3d49f491163d76b11b1332a
Victoria Malishko <vika0493@gmail.com>
2022-10-06 14:25:32 -0400

    Pretty Version of my system info

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+#creat Variables
+#variable for FQDN
+NAME=$(hostname --fqdn)
+#Variable for OS
+OS=$(hostnamectl | grep -w 'Operating System')
+#VAriable for IP addr
+IP_ADDR=$(hostname -I)
+#Variable for Space
+SPACE=$(df -h ./ | grep -w "/" | awk '{print $4}')
+#EOF command
+cat <<EOF
+Report for $NAME
+=========
+FQDN: $NAME
+$OS
+IP Address: $IP_ADDR
+Root FIlesystem Free Space: $SPACE
+=========
+EOF
 
 
 #FQDN
-hostname --fqdn
+#hostname --fqdn
+
 #The operating system name and version
-hostnamectl
+#hostnamectl
+
 #Ip addresses
-hostname -I
+#hostname -I
+
 #he amount of space available in only the root filesystem
-df -h ./
+#df -h ./
+
 
 
 exit
